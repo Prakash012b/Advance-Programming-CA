67e7dfef30aade3b81a6e53f7539fd55ea16643d
Prakash012b <148553683+Prakash012b@users.noreply.github.com>
2025-11-11 22:51:05 +0000

    setting up the netbeans
    created java class for client side and server side

---
+++ 23178523_PrakashPyakurel_Server/src/main/java/com/mycompany/_prakashpyakurel_server/BorrowRecord.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._prakashpyakurel_server;
+
+/**
+ *
+ * @author pyaku
+ */
+public class BorrowRecord {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
